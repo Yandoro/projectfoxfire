18d49853a1f449ecffdef7cbbb93630efa56e9a8
LittleVoid <08evangelion08@gmail.com>
2022-12-07 15:52:51 +0100

    Cam follows Player now

---
+++ Assets/03_Code/Scripts/MainCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public float FollowSpeed;
+    public float yOffset;
+    public Transform player;
+    public float camdistanz;
+
+    void Update()
+    {
+        //MoveCamera();
+    }
+
+    private void LateUpdate()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        Vector3 newPos = new Vector3(player.position.x, player.position.y + yOffset, camdistanz);
+        //transform.position. = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        Vector3 StoragePosition = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        transform.position = new Vector3(StoragePosition.x, player.position.y + yOffset, camdistanz);
+        //transform.position = newPos;
+    }
+
+
+}
