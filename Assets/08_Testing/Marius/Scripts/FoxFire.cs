b987b49ac742684fba1ce8b31b1570a57bd9a409
Oliver Grimm <olivergrimm89@gmail.com>
2023-01-12 12:48:03 +0100

    Foxfire Return +Aim

--- Assets/08_Testing/Marius/Scripts/FoxFire.cs
+++ Assets/08_Testing/Marius/Scripts/FoxFire.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class FoxFire : MonoBehaviour
@@ -10,10 +11,23 @@ public class FoxFire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+ 
+        StartCoroutine(ExampleCoroutine());
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
         rb.velocity = transform.right * speed;
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(.9f);
+
+        //After we have waited 5 seconds print the time again.
+        rb.velocity = transform.right * -speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+        private void OnTriggerEnter2D(Collider2D collision)
     {
 
 
