79273adc56387c678693775f09f67d4e8581c2ca
LittleVoid <08evangelion08@gmail.com>
2023-02-04 14:56:24 +0100

    Addet Cave Prefab with Animation

---
+++ Assets/CaveCover.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveCover : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] BoxCollider2D colli;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("PlayerEntering");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("PlayerLeaving");
+        }
+    }
+}
