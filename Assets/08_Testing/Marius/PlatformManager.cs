6ee0b60d7551c13772a64b775db05b4ca42e169f
LittleVoid <08evangelion08@gmail.com>
2023-02-06 18:56:03 +0100

    Changed FallPlaptform Script (now with respawn)

---
+++ Assets/08_Testing/Marius/PlatformManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour {
+
+	public static PlatformManager Instance = null;
+
+	[SerializeField] GameObject platformPrefab;
+	[SerializeField] float respawnDelay = 3.5f;
+
+	void Awake()
+	{
+		if (Instance == null) 
+			Instance = this;
+		else if (Instance != this)
+			Destroy (gameObject);
+		
+	}
+	// Use this for initialization
+	void Start () {
+		Instantiate (platformPrefab, new Vector2 (157.0f, 6.0f), platformPrefab.transform.rotation);
+	}
+
+	IEnumerator SpawnPlatform(Vector2 spawnPosition)
+	{
+		yield return new WaitForSeconds (respawnDelay);
+		Instantiate (platformPrefab, spawnPosition, platformPrefab.transform.rotation);
+	}
+
+}
